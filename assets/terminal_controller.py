ee3564286b9a4b8821b4f5b1d3862c9856569c2a
Iggy <ibarbosadsousa@gmail.com>
2026-01-01 23:09:06 -0300

    'primeiro commit automatico'

--- assets/terminal_controller.py
+++ assets/terminal_controller.py
@@ -5,11 +5,11 @@ def Do_github_commit():
     commands = [
         ["git", "add", "."],
         ["git", "commit", "-m", "'primeiro commit automatico'"],
-        ["git", "push"]
+        ["git", "push", "gitlab"]
     ]
     
     for cmd in commands:
-        
+
         resultado = subprocess.run(cmd, capture_output=True, text=True)
         print(resultado.stdout)
 
